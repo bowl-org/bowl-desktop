36ae29519a562ebe4d42440cb6e25d5c8f3d4674
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2022-10-28 19:09:45 +0300

    Electron, Vue and tailwindcss added

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld class="text-gray-300" msg="P2P Chat System"/>
+  <div class="bg-gray-300 text-sky-500 hover:font-bold"> Bold on hover </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #2D202C;
+  margin-top: 60px;
+}
+</style>
