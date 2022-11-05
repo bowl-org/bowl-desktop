a682f2ea19844d5770ed0ed8f3c1b224034cd878
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2022-11-06 01:01:46 +0300

    Online dot simplified with class binding

--- src/components/OnlineDot.vue
+++ src/components/OnlineDot.vue
@@ -1,6 +1,5 @@
 <template>
-  <span v-if="status == 'online'" class="rounded-full bg-sky-500 w-2.5 h-2.5 inline-block"/>
-  <span v-else class="rounded-full bg-red-500 w-2.5 h-2.5 inline-block"/>
+  <span class="rounded-full bg-sky-500 w-2.5 h-2.5 inline-block" :class="status == 'online' ? 'bg-sky-500' : 'bg-red-500'"/>
 </template>
 <script>
 export default {
